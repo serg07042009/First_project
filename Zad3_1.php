cd4c56eb0470efa57a475f4eb56f32041f23dc5c
Sergey <serg56063@mail.ru>
2022-03-07 00:19:58 +0300

    d

---
+++ Zad3_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+ <form method="post">
+ <div>
+ 	<input type="text" name="name"><br>
+ 	<textarea name="chat" rows="6"></textarea><br>
+ 	<input type="submit" name="submit" value="Отправить"><br><br>
+ </div>
+ </form>
+ <?php 
+    include('action.php')   
+  ?>
+</body>
+</html>
